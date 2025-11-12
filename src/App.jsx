ea008318b5b9461a6271a2835477b09351fbb8ca
dibyanshulunia25 <dibyanshulunia098@gmail.com>
2025-11-12 18:51:37 +0530

    Logo section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './sections/Hero'
 import Showcase from './sections/Showcase'
 import Navbar from './components/Navbar'
+import LogoSection from './components/LogoSection'
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <Navbar />
             <Hero />
             <Showcase />
+            <LogoSection />
         </>
     )
 }
